84e935f22f46dc67b268f2b4d8f270bd3c85f404
Jonathan Lehman <jonathan.lehman91@gmail.com>
2015-11-13 10:07:13 -0500

    Memoize processed source maps by file paths
    - Gets rid of unneeded work of reading/processing the same source map

--- src/index.js
+++ src/index.js
@@ -26,7 +26,8 @@ export function createSourceMapTransformer({
 } = {}) {
   var sourceMapTransformer = new stream.Transform({objectMode: true});
   sourceMapTransformer._transform = function(chunk, something, done){
-    var smc;
+    var lastSmc;
+    var smcCache = {};
     var transformedChunk = chunk.toString().split('\n').map(function(line) {
       if (newFileRegex.test(line)) {
         let match = line.match(newFileRegex);
@@ -35,20 +36,25 @@ export function createSourceMapTransformer({
         let lineNumber = newFileLineNumber(match);
         let columnNumber = newFileColumnNumber(match);
 
-        let rawSourceMap = getRawSourceMap(filePath);
-        smc = new SourceMapConsumer(rawSourceMap);
-        let originalPosition = smc.originalPositionFor({
+        if (!smcCache[filePath]) {
+          let rawSourceMap = getRawSourceMap(filePath);
+          lastSmc = new SourceMapConsumer(rawSourceMap);
+          smcCache[filePath] = lastSmc;
+        } else {
+          lastSmc = smcCache[filePath];
+        }
+        let originalPosition = lastSmc.originalPositionFor({
           line: lineNumber,
           column: columnNumber
         });
         return originalPositionStr(formattingSpaces, originalPosition, line);
-      } else if (prevFileRegex.test(line)) {
+      } else if (prevFileRegex.test(line) && lastSmc) {
         let match = line.match(prevFileRegex);
         let formattingSpaces = prevFileFormattingSpaces(match);
         let lineNumber = prevFileLineNumber(match);
         let columnNumber = prevFileColumnNumber(match);
 
-        let originalPosition = smc.originalPositionFor({
+        let originalPosition = lastSmc.originalPositionFor({
           line: lineNumber,
           column: columnNumber
         });
