32c48a7b10f40ee7497c1be8c3a9c071041dc9b0
Jonathan Lehman <jonathan.lehman91@gmail.com>
2017-03-21 22:59:02 -0400

    Provide unminified dist file in addition to minified

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,35 @@
 var webpack = require('webpack');
 var path = require('path');
 
-module.exports = {
+function addPlugins (obj, i) {
+  obj.plugins = [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        context: __dirname
+      },
+      minimize: true
+    })
+  ];
+  if (i === 1) {
+    obj.plugins.splice(2, 0, new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    }));
+  }
+  return obj;
+}
+
+var cfg = {
   entry: './src',
   module: {
     rules: [
@@ -16,7 +44,7 @@ module.exports = {
     ]
   },
   output: {
-    filename: 'dist/sourcemap-transformer.min.js',
+    filename: 'dist/sourcemap-transformer.js',
     libraryTarget: 'umd',
     library: 'sourcemap-transformer'
   },
@@ -24,27 +52,11 @@ module.exports = {
     extensions: ['.js'],
     modules: ['node_modules', path.join(__dirname, 'src'), __dirname]
   },
-  target: 'node',
-  plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      },
-      output: {
-        comments: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        context: __dirname
-      },
-      minimize: true
-    })
-  ]
+  target: 'node'
 };
+
+var cfgMinified = Object.assign({}, cfg); // Clone top level
+cfgMinified.output = Object.assign({}, cfg.output); // Clone deeper level
+cfgMinified.output.filename = cfgMinified.output.filename.replace(/\.js$/, '.min.js');
+
+module.exports = [cfg, cfgMinified].map(addPlugins);
